60dbba2c4b9115ec54b317042584b42502b180b2
Connor Ahearn <connor.ahearn305@gmail.com>
2018-11-02 15:55:48 -0400

    Wow scanner

--- src/yec_prep/fileReader.java
+++ src/yec_prep/fileReader.java
@@ -1,5 +1,15 @@
 package yec_prep;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class fileReader {
 
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner reader = new Scanner(new File("input.txt"));
+		
+		while(reader.hasNext()) System.out.println(reader.nextLine());
+	}
 }
