c56cd5c05b08a6d99e8d70e59e228d469169bf9d
Connor Ahearn <connor.ahearn305@gmail.com>
2018-11-02 16:57:14 -0400

    Simple gui less simple now

--- src/yec_prep/SampleGUI.java
+++ src/yec_prep/SampleGUI.java
@@ -1,24 +1,40 @@
 package yec_prep;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+//Idk what this line does but it gets rid of an error
+@SuppressWarnings("serial")
+
 public class SampleGUI extends JFrame {
+	
+	//Attributes
+	JPanel mainPane = new JPanel();
+	JButton button = new JButton("Test Button");
+	JLabel label = new JLabel("Wow a label");
 
 	public SampleGUI() {
-
+		
+		//Basic JFrame settings
 		setTitle("Simple example");
 		setSize(300, 200);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel mainPane = new JPanel();
-		JButton button = new JButton("Test Button");
 		setContentPane(mainPane);
+		
+		//Basic mainPane settings
+		mainPane.setLayout(new FlowLayout());
 		mainPane.add(button);
+		mainPane.add(label);
 	}
 
 	public static void main(String[] args) {
+		
+		//Initializing the GUI at runtime
 		SampleGUI ex = new SampleGUI();
 		ex.setVisible(true);
 	}
